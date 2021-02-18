11dafe06dbc6132b18b8aa864c07318c11382ed3
Roman <roman084@gmx.com>
2021-02-18 15:46:28 +0200

    update

--- src/components/ResumePage/ResumePage.jsx
+++ src/components/ResumePage/ResumePage.jsx
@@ -5,7 +5,7 @@ const ResumePage = () => {
 
   return (
       <div className={styles.container}>
-        <iframe className={styles.cv} src="//drive.google.com/file/d/1q1rl19XBOvSbdd71HBHAocUP36uF_bjV/preview" width="auto" height="auto" title='cv'/>
+        <iframe className={styles.cv} src="//drive.google.com/file/d/1Dfz3EtJxlSBOimfKURB6fFRcn3p8hyPa/view?usp=sharing" width="auto" height="auto" title='cv'/>
       </div>
   )
 }
